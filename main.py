165b099db05c7ca1a55493aa3c020c0cf744495f
SaraElsaggan <sara.mahmoud.sm600@gmail.com>
2023-12-22 04:11:26 +0200

    init_canvas_instead_of_tbls

---
+++ main.py
@@ -0,0 +1,60 @@
+from scipy import signal
+from scipy.signal import gaussian
+from PyQt5.QtCore import QTimer
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from PyQt5.QtWidgets import  QApplication, QMainWindow, QShortcut, QFileDialog , QSplitter , QFrame , QSlider
+from scipy.signal import spectrogram
+from scipy.signal import resample
+import sys
+from PyQt5.QtGui import QIcon, QKeySequence
+from mainwindow import Ui_MainWindow  
+from pyqtgraph import PlotWidget, ROI
+
+import numpy as np
+import pandas as pd
+from scipy.io import wavfile
+import pyqtgraph as pg
+from scipy.fftpack import rfft, rfftfreq, irfft , fft , fftfreq
+from PyQt5.QtCore import pyqtSlot
+import sounddevice as sd
+
+
+class MplCanvas(FigureCanvasQTAgg):
+    
+    def __init__(self, parent=None, width=5, height=1, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
+    
+
+ 
+class MyWindow(QMainWindow):   
+    
+    def __init__(self ):
+        super(MyWindow , self).__init__()
+      
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)  
+        self.layout_prob_sentence = MplCanvas(self)
+        self.layout_prob_user = MplCanvas(self)
+        self.spectrogram_canvas = MplCanvas(self)
+        self.ui.layout_spectogram_2.addWidget(self.spectrogram_canvas)
+        self.ui.layout_prob_sentence_2.addWidget(self.layout_prob_sentence)
+        self.ui.layout_prob_user_2.addWidget(self.layout_prob_user)
+        
+
+def main():
+    app = QApplication(sys.argv)
+    window = MyWindow() 
+   
+   
+    window.showMaximized()
+    window.show()
+    
+    sys.exit(app.exec_())
+    
+
+if __name__ == '__main__':
+    main()
